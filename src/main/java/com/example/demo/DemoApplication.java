432f6dcd8178ce7202a976aaf03ac431fb16630b
何玮 <hewei@raiyee.com>
2020-04-17 17:30:50 +0800

    add rsyslog

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
+    
+    private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        logger.debug("项目启动完毕...");
+        logger.info("项目启动完毕...");
+        logger.warn("项目启动完毕...");
+        logger.error("项目启动完毕...");
     }
 
     @RequestMapping("/")
-    public String hello(){
+    public String hello(){   
         return "hello docker";
     }
 
